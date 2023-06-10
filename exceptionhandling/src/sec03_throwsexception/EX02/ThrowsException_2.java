87c096487d71f8669cfbb18b77489d9adc25c04d
SuperbatP <wjssnfl0504@gmail.com>
2023-06-10 21:27:12 +0900

    최상위 메서드(main)가 예외 전가

---
+++ exceptionhandling/src/sec03_throwsexception/EX02/ThrowsException_2.java
@@ -0,0 +1,8 @@
+package sec03_throwsexception.EX02;
+
+public class ThrowsException_2 {
+public static void main(String[] args) throws ClassNotFoundException {
+	Class cls = Class.forName("java.lang.Objact2");
+	//jvm에게까지 전가를 하면 그냥 종료하고 오류출력
+}
+}
